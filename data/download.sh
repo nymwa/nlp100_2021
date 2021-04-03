bb546b8cb3645443de29693dc3b16f832f638696
nymwa <nymwa0@gmail.com>
2021-04-03 14:17:26 +0900

    04 05

--- data/download.sh
+++ data/download.sh
@@ -5,6 +5,8 @@ cd $(dirname $0)
 wget https://nlp100.github.io/data/popular-names.txt
 wget https://nlp100.github.io/data/jawiki-country.json.gz
 wget https://nlp100.github.io/data/neko.txt
+wget https://nlp100.github.io/data/ai.ja.zip
+unzip ai.ja.zip
 wget https://archive.ics.uci.edu/ml/machine-learning-databases/00359/NewsAggregatorDataset.zip
 wget http://download.tensorflow.org/data/questions-words.txt
 wget http://www.gabrilovich.com/resources/data/wordsim353/wordsim353.zip
